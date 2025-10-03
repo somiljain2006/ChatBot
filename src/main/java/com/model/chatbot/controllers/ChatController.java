b086706f93b4f0284ffdec073a060a48ed96abfe
somiljain2006 <somil16022006@gmail.com>
2025-10-04 00:57:29 +0530

    Added GET request for chat history

--- src/main/java/com/model/chatbot/controllers/ChatController.java
+++ src/main/java/com/model/chatbot/controllers/ChatController.java
@@ -1,4 +1,23 @@
 package com.model.chatbot.controllers;
 
+import com.model.chatbot.entity.Message;
+import com.model.chatbot.repo.MessageRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+import java.util.List;
+
+@RestController
+@RequestMapping("/chats")
+@RequiredArgsConstructor
 public class ChatController {
+
+  private final MessageRepository messageRepository;
+
+  @GetMapping("/history/{roomId}")
+  public List<Message> history(@PathVariable String roomId) {
+    return messageRepository.findAllByRoomId(roomId);
+  }
 }
