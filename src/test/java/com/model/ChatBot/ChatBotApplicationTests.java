a88ac141d8595f4e3d33de7796e76e91a2865530
somiljain2006 <somil16022006@gmail.com>
2025-10-03 15:43:16 +0530

    Add IntelliJ project files

---
+++ src/test/java/com/model/ChatBot/ChatBotApplicationTests.java
@@ -0,0 +1,13 @@
+package com.model.ChatBot;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ChatBotApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
