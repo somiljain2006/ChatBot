49a54996b19a86194e2091c2a8912d0b09f8872a
somiljain2006 <somil16022006@gmail.com>
2025-10-03 23:13:17 +0530

    Added entities

--- src/main/java/com/model/chatbot/entity/Message.java
+++ src/main/java/com/model/chatbot/entity/Message.java
@@ -1,4 +1,27 @@
 package com.model.chatbot.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "messages")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Message {
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  private Long id;
+  private String roomId;
+  private String sender;
+  private String receiver;
+  private String content;
+  private Long timestamp;
+  private boolean delivered;
 }
--- src/main/java/com/model/chatbot/entity/Room.java
+++ src/main/java/com/model/chatbot/entity/Room.java
@@ -1,4 +1,20 @@
 package com.model.chatbot.entity;
 
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Entity
+@Table(name = "rooms")
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
 public class Room {
+  @Id
+  private String id;
+  private String userA;
+  private String userB;
 }
