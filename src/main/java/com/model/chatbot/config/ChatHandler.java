fd6fe362a01e81f1fee0dbdc8ba59898995d5987
somiljain2006 <somil16022006@gmail.com>
2025-10-04 02:38:00 +0530

    Added WebSocket

---
+++ src/main/java/com/model/chatbot/config/ChatHandler.java
@@ -0,0 +1,37 @@
+package com.model.chatbot.config;
+
+import lombok.RequiredArgsConstructor;
+import org.springframework.lang.NonNull;
+import org.springframework.stereotype.Component;
+import org.springframework.web.socket.CloseStatus;
+import org.springframework.web.socket.TextMessage;
+import org.springframework.web.socket.WebSocketSession;
+import org.springframework.web.socket.handler.TextWebSocketHandler;
+import java.util.HashSet;
+import java.util.Set;
+
+@Component
+@RequiredArgsConstructor
+public class ChatHandler extends TextWebSocketHandler {
+
+  private static final Set<WebSocketSession> sessions = new HashSet<>();
+
+  @Override
+  public void afterConnectionEstablished(@NonNull WebSocketSession session) {
+    sessions.add(session);
+  }
+
+  @Override
+  protected void handleTextMessage(@NonNull WebSocketSession session, @NonNull TextMessage message) throws Exception {
+    for (WebSocketSession webSocketSession : sessions) {
+      if (webSocketSession.isOpen()) {
+        webSocketSession.sendMessage(message);
+      }
+    }
+  }
+
+  @Override
+  public void afterConnectionClosed(@NonNull WebSocketSession session, @NonNull CloseStatus status) {
+    sessions.remove(session);
+  }
+}
--- src/main/java/com/model/chatbot/config/WebSocketConfig.java
+++ src/main/java/com/model/chatbot/config/WebSocketConfig.java
@@ -1,4 +1,21 @@
 package com.model.chatbot.config;
 
-public class WebSocketConfig {
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.socket.config.annotation.EnableWebSocket;
+import org.springframework.web.socket.config.annotation.WebSocketConfigurer;
+import org.springframework.web.socket.config.annotation.WebSocketHandlerRegistry;
+
+@Configuration
+@EnableWebSocket
+@RequiredArgsConstructor
+public class WebSocketConfig implements WebSocketConfigurer {
+
+  private final ChatHandler chatHandler;
+
+  @Override
+  public void registerWebSocketHandlers(WebSocketHandlerRegistry registry) {
+    registry.addHandler(chatHandler, "/ws/chat")
+      .setAllowedOrigins("*");
+  }
 }
