219af4fe5b40e4d2c20367f091108a59bc241877
somiljain2006 <somil16022006@gmail.com>
2025-10-04 15:43:18 +0530

    Removed unecessary files

--- src/main/java/com/model/chatbot/repo/MessageRepository.java
+++
@@ -1,8 +0,0 @@
-package com.model.chatbot.repo;
-
-import com.model.chatbot.entity.Message;
-import org.springframework.data.jpa.repository.JpaRepository;
-import java.util.List;
-public interface MessageRepository extends JpaRepository<Message, Long> {
-  List<Message> findAllByRoomId(String roomId);
-}
--- src/main/java/com/model/chatbot/repo/RoomRepository.java
+++
@@ -1,6 +0,0 @@
-package com.model.chatbot.repo;
-
-import com.model.chatbot.entity.Room;
-import org.springframework.data.jpa.repository.JpaRepository;
-public interface RoomRepository extends JpaRepository<Room, String> {
-}
